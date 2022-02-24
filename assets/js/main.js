10368278b4d9c97fc6f7ec0a6a896a81350a3092
Vivian Leung <leungv99@gmail.com>
2022-02-23 18:48:33 -0800

    re-structured "content" folder, removed several sub-sections, and edited several sub-sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,18 +122,6 @@
 
 })(jQuery);
 
-// reference: https://stackoverflow.com/questions/24887258/highlight-navigation-link-as-i-scroll-down-the-page (user: mdesdev)
-
-// when the mouse is over a section, the corresponding nav item will be underlined
-$("section").mouseover(function(){
-		$('nav a[href="#'+$(this).attr('id')+'"]').addClass('focus');
-});
-
-// when the mouse leaves a section, the corresponding nav item will no longer be underlined
-$("section").mouseleave(function(){
-		$('nav a[href="#'+$(this).attr('id')+'"]').removeClass('focus');
-});
-
 // when the menu bars are clicked, show the x
 $("#menu-bars").click(function() {
 	$("#menu-bars").hide();
